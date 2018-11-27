830a169f0f944d0927f38cccc79fbb7a0f90ca2d
DmitriyMaslovN <44729773+DmitriyMaslovN@users.noreply.github.com>
2018-11-27 19:54:38 +0200

    Updates

---
+++ precache-manifest.510e39d03b353ee24f48f90caa576160.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d030a6f898d5621cb50",
+    "url": "/one-app/static/css/main.60360f9c.chunk.css"
+  },
+  {
+    "revision": "4d030a6f898d5621cb50",
+    "url": "/one-app/static/js/main.4d030a6f.chunk.js"
+  },
+  {
+    "revision": "ed24e8eec361e53ba835",
+    "url": "/one-app/static/js/1.ed24e8ee.chunk.js"
+  },
+  {
+    "revision": "2aea6386c8c022b3d779",
+    "url": "/one-app/static/js/runtime~main.2aea6386.js"
+  },
+  {
+    "revision": "5279e3e649377af1d322e6aa2ba53221",
+    "url": "/one-app/index.html"
+  }
+];
